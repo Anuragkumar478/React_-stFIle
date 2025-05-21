60fd5f94912e896f121e6e897357b5f2e705fbe1
Anurag kumar <kumaranurag9795@gmail.com>
2025-05-21 19:36:02 +0530

    Ignore backend folder as it's a separate repo

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,47 @@ import { useState } from 'react'
 
 
 import './App.css'
+import axios from 'axios'
+
+ 
 
 function App() {
-  
+  const[formData,setFormData]=useState({
+    name:'',
+    rollno:'',
+    class:'',
+    email:'',
+    registration:''
+  })
+  const handlChange=(e)=>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+  const handleSubmit=async()=>{
+    try{
+      const response= await axios.post('http://localhost:5000/submit', formData);
+      alert(response.data.message);
+    }
+    catch(error){
+     
+    }
+  }
 
   return (
     <>
       <div id='main_div'>
     <div id='form_div'>
       <form id='form'>
-      <label>Name </label><br/>
-      <input type='text'></input><br/>
+      <label >Name </label><br/>
+      <input  name='name'  type='text' onChange={handlChange}></input><br/>
       <label>Roll No</label><br/>
-      <input type='number'></input><br/>
+      <input name='rollno' type='number' onChange={handlChange}></input><br/>
       <label>class</label><br/>
-      <input type='text'></input><br/>
-      <label >gmail</label><br/>
-      <input type='email'></input><br/>
+      <input  name='class' type='text' onChange={handlChange}></input><br/>
+      <label >email</label><br/>
+      <input name='email' type='email' onChange={handlChange}></input><br/>
        <label >Registration</label><br/>
-      <input type='number'></input><br/>
-     <button type='button'>Submit</button>
+      <input name='registration' type='number' onChange={handlChange}></input><br/>
+     <button type='button' onClick={handleSubmit}>Submit</button>
       </form>
     </div>
       </div>
